1c161f2a0605eaf6995f5458bb4b82287c80bdfa
Hubert Léo <leohub@live.fr>
2018-06-25 20:27:09 +0200

    Fix header dropdown

--- jointures/header_admin.php
+++ jointures/header_admin.php
@@ -5,10 +5,10 @@ $updater = new GitRepository();
 ?>
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="/assets/js/bootstrap.min.js"></script>
+<head>
+    <link href="assets/plugins/custombox/dist/custombox.min.css" rel="stylesheet">
+</head>
 
-<script src="/assets/js/waiting.js"></script>
 
 <header id="topnav">
     <div class="topbar-main">
