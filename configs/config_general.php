cf2e75c6fb8da0cdf887199545515d61638ff307
FlashModz <manet.jeremy@hotmail.be>
2019-02-01 20:40:59 +0100

    Added Bash Var
    Added Bash Var for perform analytic stats

--- configs/config_general.php
+++ configs/config_general.php
@@ -4,3 +4,4 @@ $site = "A3 Launcher";
 $encrypt_key1 = "ABrbn+HUHJSAqnkA8+uyvWAK43yU2RtjeG+g2l65JY4=";
 $encrypt_key2 = "f97uFHDOQgodd3OczNG35NQWl2Xve3Q2xKSBbIrQH6BDpxSEmCjOfA+ubeXMHnjcnjaatpMvKyNCl7Peh5mfSA==";
 $analytics = true;
+$bash = true;
